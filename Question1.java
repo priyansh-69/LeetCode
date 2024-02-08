63b7db702b93a4d47b1ef48ff4f60bd07967c122
Priyansh Kandwal <153702794+priyansh-69@users.noreply.github.com>
2024-02-08 09:39:50 +0530

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+   for(int i=0;i<nums.length;i++){
+       for(int j=1+i;j<nums.length;j++){
+           if(nums[i]+nums[j]==target){
+               int a[]={i,j};
+               return a;
+           }
+       }
+       
+   }
+   return null ;
+}
+}
