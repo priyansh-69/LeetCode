628d924ca617f287703a798062dbf5f1f10549ba
Priyansh Kandwal <153702794+priyansh-69@users.noreply.github.com>
2024-02-09 09:55:37 +0530

    Update Question_07.java

--- Question_07.java
+++ Question_07.java
@@ -1,1 +1,14 @@
+//REVERSE INTEGER
 
+class Solution {
+  public int reverse(int x) {
+    long ans = 0;
+
+    while (x != 0) {
+      ans = ans * 10 + x % 10;
+      x /= 10;
+    }
+
+return(ans<Integer.MIN_VALUE||ans>Integer.MAX_VALUE)?0:(int)ans;
+  }
+}
