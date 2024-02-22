3617cbe9d0880788b65f6da7c2d30d2d4431e212
Priyansh Kandwal <153702794+priyansh-69@users.noreply.github.com>
2024-02-22 10:55:32 +0530

    Update question_09.java

--- question_09.java
+++ question_09.java
@@ -1,1 +1,16 @@
+//Palindrome Number
 
+class Solution{
+    public boolean isPalindrome(int x){
+  int a=0;
+  int y=x;
+  while(x>0){
+ a=a*10+x%10;
+ x/=10;
+  }
+  if(a==y){
+      return true;
+  }
+   return false;
+    }
+}
